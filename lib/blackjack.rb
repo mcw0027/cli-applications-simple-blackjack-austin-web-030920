848673a0258d0a93c401992f9f269a6440ae8cc5
Michael Wixson <wixsonmc@gmail.com>
2020-02-20 20:54:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,8 +36,7 @@ def hit?(current_total)
   if user_input == 's'
   return current_total
   elsif user_input == 'h'
-  deal_card
-  return current_total += deal_card.to_i
+  current_total += deal_card
 else
     invalid_command()
     prompt_user()
